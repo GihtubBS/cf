efd8adf4ca4ebc03282af7f959816e3093618c36
GihtubBS <90913064+GihtubBS@users.noreply.github.com>
2022-06-03 02:28:08 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'ikuuur.herokuapp.com'
+      url.hostname = 'qiqiai.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
